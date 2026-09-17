5bd8369f32e1b2106a817c4cc028c730851668ef
Bastienia <andrexbastien@gmail.com>
2026-09-17 18:31:54 +0200

    app.py

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ with col_btn2:
     if st.button("🚪 Déconnexion du profil"):
         st.session_state.utilisateur_connecte = None
         st.rerun()
-    if not st.session_state.mode_visuel_appel:
+        if not st.session_state.mode_visuel_appel:
     for msg in st.session_state.messages:
 
     
